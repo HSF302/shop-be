e942ff4fe16ffa484101482840192916bcb74a9e
TripleH-dot <huyhoang2309.work@gmail.com>
2025-07-03 21:15:01 +0700

    Controller upload ảnh, html sửa lại đề phù hợp list và thêm ảnh và chỗ để ảnh

--- src/main/java/com/office_dress_shop/shopbackend/controller/OfficeDressController.java
+++ src/main/java/com/office_dress_shop/shopbackend/controller/OfficeDressController.java
@@ -3,9 +3,16 @@ package com.office_dress_shop.shopbackend.controller;
 import com.office_dress_shop.shopbackend.pojo.OfficeDress;
 import com.office_dress_shop.shopbackend.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller
 @RequestMapping("/officedresses")
@@ -33,13 +40,6 @@ public class OfficeDressController {
         model.addAttribute("categories", categoryService.findAll());
         model.addAttribute("addons", addonService.findAll());
         return "officedress/add";
-    }
-
-
-    @PostMapping("/add")
-    public String addOfficeDress(@ModelAttribute OfficeDress officeDress) {
-        officeDressService.save(officeDress);
-        return "redirect:/officedresses";
     }
 
     @GetMapping("/edit/{id}")
@@ -78,5 +78,19 @@ public class OfficeDressController {
                     return "officedress/detail"; // Trả về view: templates/officedress/detail.html
                 })
                 .orElse("redirect:/officedresses");
+    }
+
+    @PostMapping("/add")
+    public String addOfficeDress(@ModelAttribute OfficeDress officeDress,
+                                 @RequestParam("imageFile") MultipartFile imageFile) throws IOException {
+        String uploadDir = new ClassPathResource("static/images/uploads/").getFile().getAbsolutePath();
+        String fileName = imageFile.getOriginalFilename();
+        Path path = Paths.get(uploadDir, fileName);
+        Files.write(path, imageFile.getBytes());
+
+        officeDress.setImageUrl("/images/uploads/" + fileName);
+        officeDressService.save(officeDress);
+
+        return "redirect:/officedresses";
     }
 }
